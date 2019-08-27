df65d54fc8cf85880f315cd56add451f70ca9899
Jongmin Han <33759102+jongminHan@users.noreply.github.com>
2019-08-27 15:57:23 +0900

    Add functionality to Button

--- src/InverseMatrixCalculator.java
+++ src/InverseMatrixCalculator.java
@@ -2,14 +2,18 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 
 public class InverseMatrixCalculator {
 
 	protected Shell shell;
-	private Text text;
-	private Text text_1;
+	private Text textRow;
+	private Text textCol;
 
 	/**
 	 * Launch the application.
@@ -19,7 +23,8 @@ public class InverseMatrixCalculator {
 		try {
 			InverseMatrixCalculator window = new InverseMatrixCalculator();
 			window.open();
-		} catch (Exception e) {
+		} 
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -47,23 +52,32 @@ public class InverseMatrixCalculator {
 		shell.setSize(749, 379);
 		shell.setText("SWT Application");
 		
-		Label lblEnterTheNumber = new Label(shell, SWT.NONE);
-		lblEnterTheNumber.setBounds(114, 106, 140, 15);
-		lblEnterTheNumber.setText("Enter the number of row");
+		Label lblEnterTheRow = new Label(shell, SWT.NONE);
+		lblEnterTheRow.setBounds(114, 106, 140, 15);
+		lblEnterTheRow.setText("Enter the number of row");
 		
-		Label lblEnterTheNumber_1 = new Label(shell, SWT.NONE);
-		lblEnterTheNumber_1.setBounds(114, 157, 163, 15);
-		lblEnterTheNumber_1.setText("Enter the number of column");
+		Label lblEnterTheCol = new Label(shell, SWT.NONE);
+		lblEnterTheCol.setBounds(114, 157, 163, 15);
+		lblEnterTheCol.setText("Enter the number of column");
 		
-		text = new Text(shell, SWT.BORDER);
-		text.setBounds(317, 106, 76, 21);
+		textRow = new Text(shell, SWT.BORDER); // 행의 개수를 입력
+		textRow.setBounds(317, 106, 76, 21);
 		
-		text_1 = new Text(shell, SWT.BORDER);
-		text_1.setBounds(317, 157, 76, 21);
+		textCol = new Text(shell, SWT.BORDER); // 열의 개수를 입력
+		textCol.setBounds(317, 157, 76, 21);
 		
-		Button btnOk = new Button(shell, SWT.NONE);
+		Button btnOk = new Button(shell, SWT.NONE); // 버튼이 눌리면 주어진 행, 열의 개수에 따라 비어있는 매트릭스 생성
+		btnOk.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseUp(MouseEvent e) {
+				int row = Integer.parseInt(textRow.getText());
+				int col = Integer.parseInt(textCol.getText());
+				
+				System.out.println("Row:" + row);
+				System.out.println("Column:" + col);
+			}
+		});
 		btnOk.setBounds(449, 157, 75, 25);
 		btnOk.setText("OK");
-
 	}
 }
